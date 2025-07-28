b0ea1079f04fc9e9042619cfa1651fb232a88df1
[davidCamposM] <[udecamtres@gmail.com]>
2025-07-28 01:22:33 -0400

    readme-updated-http-added

--- src/blackjack/usecases/index.js
+++ src/blackjack/usecases/index.js
@@ -1,3 +1,4 @@
+export { crearCartaHTML } from './crear-carta-html.js'
 export { crearDeck } from './crear-deck.js';
 export { pedirCarta } from './pedir-carta.js';
 export { turnoComputadora } from './turno-computadora.js'
